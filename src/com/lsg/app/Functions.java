ea63f96f1324f76ea3173b5ada36b7c3e3f24c23
scirocco101@googlemail.com <>
2012-02-25 20:18:08 +0100

    *test - not runnable!!!!!!!!!!!11

--- src/com/lsg/app/Functions.java
+++ src/com/lsg/app/Functions.java
@@ -7,19 +7,31 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
 
 public class Functions {
+	public static final String DB_NAME            = "vertretungen";
+	public static final String DB_TABLE           = "vertretungen";
+	public static final String DB_ROWID           = "_id";
+	public static final String DB_KLASSENSTUFE    = "klassenstufe";
+	public static final String DB_KLASSE          = "klasse";
+	public static final String DB_STUNDE          = "stunde";
+	public static final String DB_VERTRETER       = "vertreter";
+	public static final String DB_LEHRER          = "lehrer";
+	public static final String DB_RAUM            = "raum";
+	public static final String DB_ART             = "art";;
+	public static final String DB_VERTRETUNGSTEXT = "vertretungstext";
+	public static final String DB_FACH            = "fach";
+	public static final String DB_DATE            = "date";
 	public static void setTheme(boolean dialog, Context context) {
 		int theme = android.R.style.Theme_Black;
 		if(Build.VERSION.SDK_INT >= 11) {
@@ -36,6 +48,7 @@ public class Functions {
 	public static void refreshVPlan(Context context) {
 		 SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
 		try {
+			Functions.testDB(context);
 			String data = URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(prefs.getString("username", ""), "UTF-8");
         	data       += "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(prefs.getString("password", ""), "UTF-8");
         	URL url = new URL("http://linux.lsg.musin.de/cp/vp_app.php");
@@ -70,5 +83,29 @@ public class Functions {
         catch(Exception e) {
 	    	Log.d("except", e.getMessage());
         }
+	}
+	public static void testDB(Context context) {
+    	try {
+    		SQLiteDatabase myDB;
+    		myDB = context.openOrCreateDatabase(DB_NAME, context.MODE_PRIVATE, null);
+    		myDB.execSQL("CREATE TABLE IF NOT EXISTS " + Functions.DB_TABLE
+    				+ " (" + Functions.DB_ROWID       + " integer primary key autoincrement,"
+    	    		+ Functions.DB_KLASSENSTUFE       + " integer,"
+    	    	    + Functions.DB_KLASSE	          + " text,"
+    	    	    + Functions.DB_STUNDE             + " integer,"
+    	    	    + Functions.DB_VERTRETER          + " text,"
+    	     	    + Functions.DB_LEHRER             + " text,"
+    	    	    + Functions.DB_RAUM               + " text,"
+    	    	    + Functions.DB_ART                + " text,"
+    	    	    + Functions.DB_VERTRETUNGSTEXT    + " text,"
+    	    	    + Functions.DB_FACH               + " text,"
+    	    	    + Functions.DB_DATE               + " text"
+    				+");");
+    		//upgrades for table
+    		/*if(myDB.getVersion() == 0) {
+    			myDB.setVersion(1);
+    		}*/
+    		myDB.close();
+        } catch (Exception e) { Log.d("db", e.getMessage());}
 	}
 }
